b5ef2abd3a898ddc40d4c935466f690a1523c9e8
tyaraaaa <90371409+tyaraslw@users.noreply.github.com>
2023-08-10 20:23:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>kucing</title>
+</head>
+<body>
+<font face="Arial">
+  <h1><u>Kucing</u></h1>
+    <img src="kucing1.jpg" width="400" height="250" /><br>
+    <br>
+    <b>Kucing</b> disebut juga <b>kucing domestik</b><font color="blue" size="1"><sup>[4][5]</font> atau <b>kucing rumah</b> (<font color="blue">nama ilmiah</font>: <i>Felis silvestris catus</i> atau <i>Felis<br>
+    catus</i>) adalah sejenis <font color="blue">mamalia karnivora</font> dari keluarga Felidae. Kata "kucing" biasanya merujuk kepada <br>
+    "kucing" yang telah dijinakkan,<font color="blue" size="1"><sup>[6]</font> tetapi bisa juga bisa merujuk kepada "<font color="blue">kucing besar</font>" seperti <font color="blue">singa</font> dan <br>
+    <font color="blue">harimau</font> yang juga termasuk jenis kucing.<br>
+    <br>
+    Kucing telah berbaur dengan kehidupan <font color="blue">manusia</font> paling tidak sejak 5.000 tahun SM, dengan ditemukannya <br> 
+    <font color="blue">kerangka</font> kucing di <font color="blue">Pulau Siprus</font>.<font color="blue" size="1"><sup>[7]</font> Sejak zaman 3.500 SM, orang <font color="blue">Mesir Kuno</font> telah menggunakan kucing <br>
+    untuk menjauhkan <font color="blue">tikus</font> atau <font color="blue">hewan pengerat</font> lain dari <font color="blue">lumbung</font> yang menyimpan hasil panen.<font color="blue" size="1"><sup>[8]</font><br>
+    <br>
+    Saat ini, kucing adalah salah satu <font color="blue">hewan peliharaan</font> terpopuler di dunia.<font color="blue" size="1"><sup>[9]</font> Kucing yang garis keturunannya <br>
+    tercatat secara resmi sebagai kucing <font color="blue">trah</font> atau <font color="blue">galur murni</font> (<i>pure breed</i>), seperti <font color="blue">persia</font>, <font color="blue">siam</font>, <font color="blue">manx</font>, dan <br>
+    <font color="blue">sphinx</font>. Kucing seperti ini biasanya dibiakkan di tempat pemeliharaan hewan resmi. Jumlah kucing ras <br>
+    hanyalah 1% dari seluruh kucing di dunia, sisanya adalah kucing dengan keturunan campuran, seperti <font color="blue">kucing <br>
+    liar</font> atau kucing kampung. Kucing juga merupakan jenis hewan peliharaan yang banyak diminati karena <br>
+    tingkah lakunya yang lucu. <br> 
+<br>
+<a href="index2.php">Kucing Besar</a>
+</font>
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kucing besar</title>
+</head>
+<body>
+<font face="Arial">
+<h1><u>Kucing besar</u></h1>
+  <img src="kucing2.jpg" width="400" height="260" /><br>
+  <br>
+  Istilah <b>kucing besar</b> - yang bukan merupakan klasifikasi biologis - digunakan untuk membedakan spesies kucing <br>
+  yang lebih besar dari yang lebih kecil. Salah satu definisi "kucing besar" termasuk empat anggota genus Panthera: <br>
+  <font color="blue">harimau</font>, <font color="blue">singa</font>, <font color="blue">jaguar</font>, dan <font color="blue">macan tutul</font>. Anggota dari genus ini adalah satu-satunya kucing bisa mengaum. <br>
+  Definisi yang lebih luas dari "kucing besar" juga mencakup <font color="blue">cheetah</font>, <font color="blue">macan tutul salju</font>, dan <font color="blue">puma</font>. <br>
+  Meskipun perbedaan besar dalam ukuran, berbagai jenis kucing yang sangat mirip dalam struktur dan perilaku <br> 
+  baik, dengan pengecualian cheetah, yang secara signifikan berbeda dari semua kucing yang besar ataupun kecil. <br> 
+  Mereka bisa ditemukan di Amerika, Afrika, Asia, dan Eropa.
+</font>
+</body>
+</html>
